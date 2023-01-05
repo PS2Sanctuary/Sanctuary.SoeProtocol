e6bcfa50d9ad80f5aea924d4ae0e93d46bc7eade
Carl Stephens <carlst99@outlook.co.nz>
2023-01-06 12:02:56 +1300

    Add NativeSpan.cs and NativeSpanPool.cs

---
+++ src/Sanctuary.SoeProtocol/Objects/NativeSpan.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Sanctuary.SoeProtocol.Objects;
+
+/// <summary>
+/// Represents a wrapper around native memory.
+/// </summary>
+public unsafe readonly struct NativeSpan
+{
+    private readonly int _len;
+    internal readonly byte* _ptr;
+
+    /// <summary>
+    /// Gets a span around the underlying native memory.
+    /// </summary>
+    public Span<byte> Span => new(_ptr, _len);
+
+    /// <summary>
+    /// Use of the default construct is invalid.
+    /// </summary>
+    /// <exception cref="InvalidOperationException">Use of the default construct is invalid.</exception>
+    public NativeSpan()
+    {
+        throw new InvalidOperationException($"Use of {nameof(NativeSpan)}'s default constructor is invalid");
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NativeSpan"/> struct.
+    /// </summary>
+    /// <param name="pointer">A pointer to the native memory to wrap.</param>
+    /// <param name="length">The length in bytes of the underlying native memory.</param>
+    public NativeSpan(byte* pointer, int length)
+    {
+        _ptr = pointer;
+        _len = length;
+    }
+
+    /// <summary>
+    /// Allocates a new <see cref="NativeSpan"/> object directly from native memory.
+    /// The underlying memory must be freed once use of the span is complete.
+    /// </summary>
+    /// <param name="length">The number of bytes to allocate.</param>
+    /// <returns>The allocated native span.</returns>
+    public static NativeSpan Allocate(int length)
+        => new((byte*)NativeMemory.Alloc((nuint)length), length);
+
+    /// <summary>
+    /// Frees the underlying memory of an allocated <see cref="NativeSpan"/> object.
+    /// </summary>
+    /// <param name="span">The <see cref="NativeSpan"/> to free.</param>
+    public static void Free(NativeSpan span)
+        => NativeMemory.Free(span._ptr);
+}
---
+++ src/Sanctuary.SoeProtocol/Util/NativeSpanPool.cs
@@ -0,0 +1,80 @@
+﻿using Sanctuary.SoeProtocol.Objects;
+using System;
+using System.Collections.Concurrent;
+using System.Runtime.InteropServices;
+
+namespace Sanctuary.SoeProtocol.Util;
+
+/// <summary>
+/// Represents a fixed-limit pool of fixed-size <see cref="NativeSpan"/> objects.
+/// </summary>
+public sealed class NativeSpanPool : IDisposable
+{
+    private readonly int _memorySize;
+    private readonly int _poolSize;
+    private readonly ConcurrentStack<nuint> _pool;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NativeSpanPool"/> class.
+    /// </summary>
+    /// <param name="memorySize">
+    /// The size in bytes of each <see cref="NativeSpan"/> object to allocate within the pool.
+    /// </param>
+    /// <param name="poolSize">
+    /// The maximum number of <see cref="NativeSpan"/> objects that may be held within the pool.
+    /// </param>
+    public NativeSpanPool(int memorySize, int poolSize)
+    {
+        _memorySize = memorySize;
+        _poolSize = poolSize;
+        _pool = new ConcurrentStack<nuint>();
+    }
+
+    /// <summary>
+    /// Rents a <see cref="NativeSpan"/> object from the pool.
+    /// Rented objects must be returned to the pool.
+    /// </summary>
+    /// <remarks>
+    /// This method will allocate a new <see cref="NativeSpan"/> object if the pool is empty.
+    /// </remarks>
+    /// <returns>The rented object.</returns>
+    public unsafe NativeSpan Rent()
+    {
+        if (_pool.TryPop(out nuint memoryPointer))
+            return new NativeSpan((byte*)memoryPointer, _memorySize);
+
+        byte* ptr = (byte*)NativeMemory.Alloc((nuint)_memorySize);
+        return new NativeSpan(ptr, _memorySize);
+    }
+
+    /// <summary>
+    /// Returns a rented <see cref="NativeSpan"/> object to the pool.
+    /// </summary>
+    /// <remarks>
+    /// The underlying memory of the <see cref="NativeSpan"/> will be freed if the pool is full.
+    /// </remarks>
+    /// <param name="span">The <see cref="NativeSpan"/> to return.</param>
+    /// <exception cref="InvalidOperationException">
+    /// Thrown if the given <see cref="NativeSpan"/> is a different length to the memory size of the pool.
+    /// </exception>
+    public unsafe void Return(NativeSpan span)
+    {
+        if (span.Span.Length != _memorySize)
+            throw new InvalidOperationException($"The {nameof(NativeSpan)} was not rented from this pool");
+
+        if (_pool.Count >= _poolSize)
+        {
+            NativeMemory.Free(span._ptr);
+            return;
+        }
+
+        _pool.Push((nuint)span._ptr);
+    }
+
+    /// <inheritdoc />
+    public unsafe void Dispose()
+    {
+        while (_pool.TryPop(out nuint nativePtr))
+            NativeMemory.Free((byte*)nativePtr);
+    }
+}
