0313bfb315aac2794a9ff85ccca3675bdeaba604
Carl Stephens <carlst99@outlook.co.nz>
2023-01-08 12:50:43 +1300

    Add the Heartbeat.cs packet

--- src/Sanctuary.SoeProtocol/Objects/Packets/Heartbeat.cs
+++ src/Sanctuary.SoeProtocol/Objects/Packets/Heartbeat.cs
@@ -4,22 +4,22 @@ using System.Buffers.Binary;
 namespace Sanctuary.SoeProtocol.Objects.Packets;
 
 /// <summary>
-/// Represents a packet used to indicate that the receiving party
-/// does not have a session associated with the sender's address.
+/// Represents a packet used to keep a session alive, when no
+/// data has been receiving by either party for some time.
 /// </summary>
-public readonly record struct UnknownSender
+public readonly record struct Heartbeat
 {
     /// <summary>
     /// Gets the buffer size required to serialize a
-    /// <see cref="UnknownSender"/> packet.
+    /// <see cref="Heartbeat"/> packet.
     /// </summary>
     public const int Size = sizeof(SoeOpCode);
 
     /// <summary>
-    /// Serializes this <see cref="UnknownSender"/> packet to a buffer.
+    /// Serializes this <see cref="Heartbeat"/> packet to a buffer.
     /// This method writes the OP code to the buffer.
     /// </summary>
     /// <param name="buffer">The buffer.</param>
     public static void Serialize(Span<byte> buffer)
-        => BinaryPrimitives.WriteUInt16BigEndian(buffer, (ushort)SoeOpCode.UnknownSender);
+        => BinaryPrimitives.WriteUInt16BigEndian(buffer, (ushort)SoeOpCode.Heartbeat);
 }
