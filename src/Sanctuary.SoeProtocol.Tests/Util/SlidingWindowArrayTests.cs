03be78e5e490b3af3fc679fa5956d1ed3239f9ff
Carl Stephens <carlst99@outlook.co.nz>
2023-01-09 18:48:21 +1300

    Add SlidingWindowArray.cs

---
+++ src/Sanctuary.SoeProtocol.Tests/Util/SlidingWindowArrayTests.cs
@@ -0,0 +1,68 @@
+﻿using Sanctuary.SoeProtocol.Util;
+
+namespace Sanctuary.SoeProtocol.Tests.Util;
+
+public class SlidingWindowArrayTests
+{
+    [Fact]
+    public void TestLength()
+    {
+        SlidingWindowArray<byte> slidingWindow = new(8);
+        Assert.Equal(8, slidingWindow.Length);
+        Assert.Equal(8, slidingWindow.GetUnderlyingArray(out _).Length);
+    }
+
+    [Fact]
+    public void TestCurrent()
+    {
+        byte[] existing = { 0, 1, 2 };
+        SlidingWindowArray<byte> slidingWindow = new(existing);
+
+        slidingWindow.Slide();
+        Assert.Equal(existing[1], slidingWindow.Current);
+    }
+
+    [Fact]
+    public void TestCtorUsingExistingArray()
+    {
+        byte[] existing = { 0, 1, 2 };
+        SlidingWindowArray<byte> slidingWindow = new(existing);
+        Assert.Equal(3, slidingWindow.Length);
+
+        byte[] underlying = slidingWindow.GetUnderlyingArray(out _);
+        Assert.Equal(existing, underlying);
+    }
+
+    [Fact]
+    public void TestIndexer()
+    {
+        byte[] existing = { 0, 1, 2 };
+        SlidingWindowArray<byte> slidingWindow = new(existing);
+
+        for (int i = 0; i < existing.Length; i++)
+            Assert.Equal(existing[i], slidingWindow[i]);
+
+        slidingWindow[1] = 5;
+        Assert.Equal(5, existing[1]);
+    }
+
+    [Fact]
+    public void TestSlide()
+    {
+        byte[] existing = { 0, 1, 2, 4, 5 };
+        SlidingWindowArray<byte> slidingWindow = new(existing);
+
+        slidingWindow.Slide(2);
+        Assert.Equal(existing[2], slidingWindow[0]);
+
+        slidingWindow.Slide();
+        Assert.Equal(existing[3], slidingWindow[0]);
+        Assert.Equal(existing[0], slidingWindow[2]);
+
+        slidingWindow.Slide(9);
+        Assert.Equal(existing[2], slidingWindow[0]);
+
+        slidingWindow.Slide(-4);
+        Assert.Equal(existing[3], slidingWindow[0]);
+    }
+}
---
+++ src/Sanctuary.SoeProtocol/Util/SlidingWindowArray.cs
@@ -0,0 +1,95 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Sanctuary.SoeProtocol.Util;
+
+/// <summary>
+/// Implements a sliding-window array, which indexes its items relative
+/// to the current position of the window on the underlying array.
+/// </summary>
+/// <typeparam name="T">The underlying type of the window array.</typeparam>
+[SkipLocalsInit]
+public class SlidingWindowArray<T>
+{
+    private readonly T[] _array;
+
+    /// <summary>
+    /// Gets the index into the <see cref="_array"/> at which the current window starts.
+    /// </summary>
+    private int _windowStart;
+
+    /// <summary>
+    /// Gets an item at the given index.
+    /// </summary>
+    /// <param name="index">The index, relative to the current window.</param>
+    /// <returns>The element at the given index.</returns>
+    public T this[int index]
+    {
+        get => _array[TranslateWindowOffsetToArrayIndex(index)];
+        set => _array[TranslateWindowOffsetToArrayIndex(index)] = value;
+    }
+
+    /// <summary>
+    /// Gets the length of the underlying array.
+    /// </summary>
+    public int Length => _array.Length;
+
+    /// <summary>
+    /// Gets the item currently exposed by the window.
+    /// </summary>
+    public T Current
+    {
+        get => this[0];
+        set => this[0] = value;
+    }
+
+    public SlidingWindowArray(int windowLength)
+        : this(new T[windowLength])
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SlidingWindowArray{T}"/> class.
+    /// </summary>
+    /// <param name="array">The underlying array to wrap.</param>
+    public SlidingWindowArray(T[] array)
+    {
+        _array = array;
+    }
+
+    /// <summary>
+    /// Shifts the start of the window.
+    /// </summary>
+    /// <param name="offset">The amount to shift the window by.</param>
+    public void Slide(int offset = 1)
+        => _windowStart = TranslateWindowOffsetToArrayIndex(offset);
+
+    /// <summary>
+    /// Gets a reference to the underlying array.
+    /// </summary>
+    /// <param name="currentWindowStartIndex">The index within the array at which the current window starts.</param>
+    /// <returns>The underlying array.</returns>
+    public T[] GetUnderlyingArray(out int currentWindowStartIndex)
+    {
+        currentWindowStartIndex = _windowStart;
+        return _array;
+    }
+
+    /// <summary>
+    /// Translates an offset from the current window position
+    /// to an index in the underlying array.
+    /// </summary>
+    /// <param name="offset">The offset from the current window position.</param>
+    /// <returns>The representative index in the underlying array.</returns>
+    private int TranslateWindowOffsetToArrayIndex(int offset)
+    {
+        int index = _windowStart + offset;
+
+        if (index < 0)
+            return Length + index % Length;
+
+        if (index >= _array.Length)
+            return index % Length;
+
+        return index;
+    }
+}
