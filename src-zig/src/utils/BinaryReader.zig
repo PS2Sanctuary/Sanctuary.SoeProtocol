1c90b3580f2a9881ab7b434f6e6a6ce4e5dab6e7
Carl <carlst99@outlook.co.nz>
2024-08-31 00:55:56 +1200

    zig: Add BinaryWriter

--- src-zig/src/utils/BinaryReader.zig
+++ src-zig/src/utils/BinaryReader.zig
@@ -22,14 +22,14 @@ pub fn advance(self: *BinaryReader, amount: usize) void {
 }
 
 /// Reads a byte value
-pub fn readByte(self: *BinaryReader) u8 {
+pub fn readU8(self: *BinaryReader) u8 {
     self.offset += 1;
     return self.slice[self.offset - 1];
 }
 
 /// Reads a boolean value. Zero is treated as false, One as true, and all other values as errors.
 pub fn readBool(self: *BinaryReader) BinaryReaderError!bool {
-    return switch (self.readByte()) {
+    return switch (self.readU8()) {
         0 => false,
         1 => true,
         else => BinaryReaderError.NonBooleanValue,
@@ -37,42 +37,23 @@ pub fn readBool(self: *BinaryReader) BinaryReaderError!bool {
 }
 
 /// Reads an unsigned 24-bit integer in big endian form.
-pub fn readUInt24BE(self: *BinaryReader) u24 {
+pub fn readU24BE(self: *BinaryReader) u24 {
     var value: u24 = 0;
-    value |= @as(u24, self.readByte()) << 16;
-    value |= @as(u16, self.readByte()) << 8;
-    value |= self.readByte();
+    value |= @as(u24, self.readU8()) << 16;
+    value |= @as(u16, self.readU8()) << 8;
+    value |= self.readU8();
     return value;
 }
 
 /// Reads an unsigned 32-bit integer in big endian form.
-pub fn readUInt32BE(self: *BinaryReader) u32 {
+pub fn readU32BE(self: *BinaryReader) u32 {
     var value: u32 = 0;
-    value |= @as(u32, self.readByte()) << 24;
-    value |= @as(u32, self.readByte()) << 16;
-    value |= @as(u16, self.readByte()) << 8;
-    value |= self.readByte();
+    value |= @as(u32, self.readU8()) << 24;
+    value |= @as(u32, self.readU8()) << 16;
+    value |= @as(u16, self.readU8()) << 8;
+    value |= self.readU8();
     return value;
 }
-
-// pub fn readStringNullTerminated(self: *BinaryReader) BinaryReaderError![:0]const u8 {
-//     // Slice up to a null-terminator (sentinel). This slice will not include the sentinel
-//     const initial_slice: []const u8 = std.mem.sliceTo(self.slice[self.offset..], 0);
-//     // Get the actual offset of the sentinel in our slice
-//     const final_offset = self.offset + initial_slice.len;
-
-//     // We need to check that a sentinel was actually found, as std.mem.sliceTo() will return
-//     // the entire slice if it can't find the sentinel value
-//     if (self.slice[final_offset] != 0) {
-//         return BinaryReaderError.StringNotTerminated;
-//     }
-
-//     // Slice our buffer and cast to a sentinel-slice
-//     const value: [:0]const u8 = self.slice[self.offset..final_offset :0];
-//     self.offset = final_offset;
-
-//     return value;
-// }
 
 pub fn readStringNullTerminated(self: *BinaryReader) BinaryReaderError![:0]const u8 {
     // Get the index of the first null-terminator (sentinel) past our offset
@@ -92,15 +73,15 @@ pub fn readStringNullTerminated(self: *BinaryReader) BinaryReaderError![:0]const
     return value;
 }
 
-test "Test BinaryReader.readByte()" {
+test readU8 {
     const data = [_]u8{ 0x1, 0xFF };
     var reader = BinaryReader.init(&data);
 
-    try std.testing.expectEqual(0x1, reader.readByte());
-    try std.testing.expectEqual(0xFF, reader.readByte());
+    try std.testing.expectEqual(0x1, reader.readU8());
+    try std.testing.expectEqual(0xFF, reader.readU8());
 }
 
-test "Test BinaryReader.readBool()" {
+test readBool {
     const data = [_]u8{ 0x0, 0x1, 0xFF };
     var reader = BinaryReader.init(&data);
 
@@ -109,23 +90,23 @@ test "Test BinaryReader.readBool()" {
     try std.testing.expectError(BinaryReaderError.NonBooleanValue, reader.readBool());
 }
 
-test "Test BinaryReader.readUInt24BE()" {
+test readU24BE {
     const data = [_]u8{ 0x01, 0x00, 0x00, 0x01, 0x00, 0x63 };
     var reader = BinaryReader.init(&data);
 
-    try std.testing.expectEqual(std.math.maxInt(u16) + 1, reader.readUInt24BE());
-    try std.testing.expectEqual(std.math.maxInt(u16) + 100, reader.readUInt24BE());
+    try std.testing.expectEqual(std.math.maxInt(u16) + 1, reader.readU24BE());
+    try std.testing.expectEqual(std.math.maxInt(u16) + 100, reader.readU24BE());
 }
 
-test "Test BinaryReader.readUInt32BE()" {
+test readU32BE {
     const data = [_]u8{ 0x00, 0x01, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF };
     var reader = BinaryReader.init(&data);
 
-    try std.testing.expectEqual(std.math.maxInt(u16) + 1, reader.readUInt32BE());
-    try std.testing.expectEqual(std.math.maxInt(u32), reader.readUInt32BE());
+    try std.testing.expectEqual(std.math.maxInt(u16) + 1, reader.readU32BE());
+    try std.testing.expectEqual(std.math.maxInt(u32), reader.readU32BE());
 }
 
-test "Test BinaryReader.readStringNullTerminated()" {
+test readStringNullTerminated {
     const data = "testString";
     // Coerce to slice with sentinel included
     var reader = BinaryReader.init(data[0 .. data.len + 1]);
---
+++ src-zig/src/utils/BinaryWriter.zig
@@ -0,0 +1,118 @@
+/// A sequential writer of primitives to binary data
+const BinaryWriter = @import("BinaryWriter.zig");
+const std = @import("std");
+
+/// The underlying slice of binary data.
+slice: []u8,
+/// The offset into the slice that the reader is at.
+offset: usize = 0,
+
+pub fn init(slice: []u8) BinaryWriter {
+    return BinaryWriter{ .slice = slice };
+}
+
+/// Advances the offset of the writer by the given amount.
+pub fn advance(self: *BinaryWriter, amount: usize) void {
+    self.offset += amount;
+}
+
+/// Writes a byte value
+pub fn writeU8(self: *BinaryWriter, value: u8) void {
+    self.slice[self.offset] = value;
+    self.offset += @sizeOf(u8);
+}
+
+/// Writes a boolean value. False is treated as a zero, and true as a one.
+pub fn writeBool(self: *BinaryWriter, value: bool) void {
+    self.writeU8(switch (value) {
+        false => 0,
+        true => 1,
+    });
+}
+
+/// Writes an unsigned 16-bit integer in big endian form.
+pub fn writeU16BE(self: *BinaryWriter, value: u16) void {
+    self.writeU8(@truncate(value >> 8));
+    self.writeU8(@truncate(value));
+}
+
+/// Writes an unsigned 24-bit integer in big endian form.
+pub fn writeU24BE(self: *BinaryWriter, value: u24) void {
+    self.writeU8(@truncate(value >> 16));
+    self.writeU8(@truncate(value >> 8));
+    self.writeU8(@truncate(value));
+}
+
+/// Writes an unsigned 32-bit integer in big endian form.
+pub fn writeU32BE(self: *BinaryWriter, value: u32) void {
+    self.writeU8(@truncate(value >> 24));
+    self.writeU8(@truncate(value >> 16));
+    self.writeU8(@truncate(value >> 8));
+    self.writeU8(@truncate(value));
+}
+
+pub fn writeStringNullTerminated(self: *BinaryWriter, value: [:0]const u8) void {
+    // Copy the string slice, including the sentinel, into our buffer
+    std.mem.copyForwards(u8, self.slice[self.offset..], value[0 .. value.len + 1]);
+    self.offset += value.len + 1;
+}
+
+test writeU8 {
+    var data: [2]u8 = undefined;
+    var writer = BinaryWriter.init(&data);
+    writer.writeU8(0x1);
+    writer.writeU8(0xFF);
+
+    try std.testing.expectEqual(0x1, data[0]);
+    try std.testing.expectEqual(0xFF, data[1]);
+}
+
+test writeBool {
+    var data: [2]u8 = undefined;
+    var writer = BinaryWriter.init(&data);
+    writer.writeBool(false);
+    writer.writeBool(true);
+
+    try std.testing.expectEqual(0, data[0]);
+    try std.testing.expectEqual(1, data[1]);
+}
+
+test writeU16BE {
+    var data: [4]u8 = undefined;
+    var writer = BinaryWriter.init(&data);
+    writer.writeU16BE(std.math.maxInt(u8));
+    writer.writeU16BE(std.math.maxInt(u16));
+
+    try std.testing.expectEqualSlices(u8, &[_]u8{ 0x00, 0xFF }, data[0..2]);
+    try std.testing.expectEqualSlices(u8, &[_]u8{ 0xFF, 0xFF }, data[2..4]);
+}
+
+test writeU24BE {
+    var data: [6]u8 = undefined;
+    var writer = BinaryWriter.init(&data);
+    writer.writeU24BE(std.math.maxInt(u8));
+    writer.writeU24BE(std.math.maxInt(u24) - 1);
+
+    try std.testing.expectEqualSlices(u8, &[_]u8{ 0x00, 0x00, 0xFF }, data[0..3]);
+    try std.testing.expectEqualSlices(u8, &[_]u8{ 0xFF, 0xFF, 0xFE }, data[3..6]);
+}
+
+test writeU32BE {
+    var data: [8]u8 = undefined;
+    var writer = BinaryWriter.init(&data);
+    writer.writeU32BE(std.math.maxInt(u16) + 1);
+    writer.writeU32BE(std.math.maxInt(u32));
+
+    try std.testing.expectEqualSlices(u8, &[_]u8{ 0x00, 0x01, 0x00, 0x00 }, data[0..4]);
+    try std.testing.expectEqualSlices(u8, &[_]u8{ 0xFF, 0xFF, 0xFF, 0xFF }, data[4..8]);
+}
+
+test writeStringNullTerminated {
+    var data: [12]u8 = undefined;
+    var writer = BinaryWriter.init(&data);
+    writer.writeStringNullTerminated("test");
+    writer.writeStringNullTerminated("String");
+
+    // Coerce string to slice with sentinel included
+    try std.testing.expectEqualSlices(u8, "test\x00String"[0..12], &data);
+}
