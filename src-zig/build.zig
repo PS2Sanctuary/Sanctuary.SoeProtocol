c75e3a7609118cc4a4080f0196079f2f25b04ad7
Carl Stephens <carlst99@outlook.co.nz>
2024-10-20 22:26:37 +1300

    Add zig-network dependency

--- src-zig/build.zig
+++ src-zig/build.zig
@@ -24,6 +24,9 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    // Add 3rd-party dependencies as modules
+    lib.root_module.addImport("network", b.dependency("network", .{}).module("network"));
+
     // This declares intent for the library to be installed into the standard
     // location when the user invokes the "install" step (the default step when
     // running `zig build`).
