bddf0111bd982e175c1d8792960267e396e96d22
Carl Stephens <carlst99@outlook.co.nz>
2025-02-28 23:15:00 +1300

    Fix acks only walking up to the current sequence, which may have been reverted _behind_ the ack, preventing the walkup and stalling the channel

--- src-cs/Sanctuary.SoeProtocol/Services/ReliableDataOutputChannel.cs
+++ src-cs/Sanctuary.SoeProtocol/Services/ReliableDataOutputChannel.cs
@@ -222,8 +222,8 @@ public sealed class ReliableDataOutputChannel : IDisposable
             OutputStats.ActualAcknowledgeCount++;
         }
 
-        // Walk the window forward to our _currentSequence, until we find a packet that hasn't been acked
-        while (_windowStartSequence < _currentSequence)
+        // Walk the window forward to our _totalSequence, until we find a packet that hasn't been acked
+        while (_windowStartSequence < _totalSequence)
         {
             index = GetSequenceIndexInDispatchBuffer(_windowStartSequence);
             if (_dispatchStash[index].DataSpan is not null)
